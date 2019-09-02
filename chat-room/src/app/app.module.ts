5fb4e73d048a8f764835dd3583c387bbe9e519c0
TengHong <h176022576t@gmail.com>
2019-09-03 03:43:04 +1000

    add all the basic vanila components and services

--- chat-room/src/app/app.module.ts
+++ chat-room/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupsComponent } from './groups/groups.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { GroupDetailComponent } from './group-detail/group-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupsComponent,
+    UsersComponent,
+    LoginComponent,
+    LogoutComponent,
+    UserDetailComponent,
+    GroupDetailComponent
   ],
   imports: [
     BrowserModule,
